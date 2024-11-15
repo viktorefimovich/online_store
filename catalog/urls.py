1e48bd24e01ee5b19e5318399af6d10f704e9eba
viktorefimovich <viktorefimovich@bk.ru>
2024-11-15 12:49:56 +0700

    Добавил маршрутизацию

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home
+from catalog.views import home, contacts
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('', contacts, name='contacts'),
 ]
