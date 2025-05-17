7153101b935f62b73b01dc673497743f96b981ec
Viktor Vershinin <viktorefimovich@bk.ru>
2025-05-17 16:34:52 +0300

    Исправлено

--- config/settings.py
+++ config/settings.py
@@ -108,3 +108,7 @@ STATICFILES_DIRS = (BASE_DIR / "static",)
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = "media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
