51e5578e85719c6054fd0a78d244e08ed03dc02e
Viktor Vershinin <viktorefimovich@bk.ru>
2025-05-18 11:04:58 +0300

    Исправлено black

--- catalog/management/commands/add_products.py
+++ catalog/management/commands/add_products.py
@@ -9,17 +9,29 @@ class Command(BaseCommand):
         category, _ = Category.objects.get_or_create(name="Смартфон")
 
         products = [
-            {"name": "Samsung S25", "description": "Современный телефон", "price": "70000", "category": category,
-             "created_at": "2025-04-01"},
-
-            {"name": "Apple iPhone 16 Pro", "description": "Современный телефон", "price": "100000",
-             "category": category, "created_at": "2025-02-01"},
+            {
+                "name": "Samsung S25",
+                "description": "Современный телефон",
+                "price": "70000",
+                "category": category,
+                "created_at": "2025-04-01",
+            },
+            {
+                "name": "Apple iPhone 16 Pro",
+                "description": "Современный телефон",
+                "price": "100000",
+                "category": category,
+                "created_at": "2025-02-01",
+            },
         ]
 
         for product_data in products:
             product, created = Product.objects.get_or_create(**product_data)
             if created:
-                self.stdout.write(self.style.SUCCESS(f"Successfully added product: {product.name}"))
+                self.stdout.write(
+                    self.style.SUCCESS(f"Successfully added product: {product.name}")
+                )
             else:
-                self.stdout.write(self.style.WARNING(f"Product already exist: {product.name}"))
-
+                self.stdout.write(
+                    self.style.WARNING(f"Product already exist: {product.name}")
+                )
