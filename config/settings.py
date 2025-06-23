4851435c690d7f2c6b692815a840c37a0413df23
Viktor Vershinin <viktorefimovich@bk.ru>
2025-06-23 21:20:33 +0700

    Создано приложение users Добавлена модель User Дополнен файл settings.py Применены миграции

--- config/settings.py
+++ config/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "catalog",
     "blog",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -114,3 +115,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = "media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+AUTH_USER_MODEL = "users.User"
