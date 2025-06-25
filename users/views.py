912090655f53d628854aa47117a3531e441e062d
Viktor Vershinin <viktorefimovich@bk.ru>
2025-06-25 21:43:13 +0700

    Исправлено

--- users/views.py
+++ users/views.py
@@ -1,14 +1,10 @@
-import secrets
-
+from django.contrib.auth import login
 from django.core.mail import send_mail
-from django.shortcuts import get_object_or_404, redirect
-from django.urls import reverse_lazy, reverse
+from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 from users.forms import UserRegisterForm
 from users.models import User
-
-from config.settings import EMAIL_HOST_USER
 
 
 class UserCreateView(CreateView):
@@ -18,23 +14,13 @@ class UserCreateView(CreateView):
 
     def form_valid(self, form):
         user = form.save()
-        user.is_active = False
-        token = secrets.token_hex(16)
-        user.token = token
-        user.save()
-        host = self.request.get_host
-        url = f"http://{host}/users/email-confirm/{token}/"
-        send_mail(
-            subject="Подтверждение почты",
-            message=f"Привет, перейди по ссылке для подтверждения почты {url}",
-            from_email=EMAIL_HOST_USER,
-            recipient_list=[user.email],
-        )
+        login(self.request, user)
+        self.send_welcome_email(user.email)
         return super().form_valid(form)
 
-
-def email_verification(request, token):
-    user = get_object_or_404(User, token=token)
-    user.is_active = True
-    user.save()
-    return redirect(reverse("users:login"))
+    def send_welcome_email(self, user_email):
+        subject = 'Добро пожаловать в наш сервис'
+        message = 'Спасибо, что зарегистрировались в нашем сервисе!'
+        from_email = 'your_email@yandex.ru'
+        recipient_list = [user_email]
+        send_mail(subject, message, from_email, recipient_list)
