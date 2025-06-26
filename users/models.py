1c26c098eed9dc8f4d9efd7c3213ffa025b6002c
Viktor Vershinin <viktorefimovich@bk.ru>
2025-06-26 14:42:42 +0700

    Исправлено

--- users/models.py
+++ users/models.py
@@ -11,8 +11,6 @@ class User(AbstractUser):
     country = models.CharField(max_length=70, verbose_name="Страна", blank=True, null=True,
                                help_text="Введите свою страну")
 
-    token = models.CharField(max_length=100, verbose_name="Token", blank=True, null=True)
-
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
